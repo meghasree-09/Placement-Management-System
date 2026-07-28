8d6d25da4042a48ea326c884a0c046ee0fd13186
meghasree-09 <saimeghanasree09@gmail.com>
2026-07-28 23:19:23 +0530

    enhanced app configurations

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,9 @@ import Login from './pages/Login/Login'
 import { Routes ,Route} from 'react-router-dom'
 import Layout from './components/Layout/Layout'
 import Companies from "./pages/Companies/Companies";
+import Placement from './pages/Placements/Placement';
+import Reports from './pages/Reports/Reports';
+import Settings from './pages/Settings/Settings'
 import Students from "./pages/Students/Students";
 import NotFound from "./pages/NotFound/NotFound";
 import EditStudent from "./pages/EditStudent/EditStudent";
@@ -94,6 +97,9 @@ function App(){
         <Dashboard/>
         </ProtectedRoute>}/>
     <Route path="/companies" element={<Companies />} />
+    <Route path="/placements" element={<Placement/>}/>
+    <Route path="/reports" element={<Reports/>}/>
+    <Route path='/settings' element={<Settings/>}/>
     
 
     <Route path="/students" element={
